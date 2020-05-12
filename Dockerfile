1ff29eb3e83a4d38f3b9ace9d1c2fc0e2566946c
Vladimir Stafievsky <justdewil@gmail.com>
2020-05-12 11:45:59 +0300

    Autoreloader added

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM nginx:stable
+
+RUN apt-get update && apt-get install -y supervisor
+
+RUN apt-get clean && rm -rf /var/cache/apt/archives/*
+
+ADD nginx_supervisord /etc/supervisor/conf.d/nginx_supervisord.conf
+
+ADD checksha.sh /usr/local/bin/checksha
+
+RUN chmod +x /usr/local/bin/checksha
+
+ENTRYPOINT ["/usr/bin/supervisord"]
+
+STOPSIGNAL SIGQUIT
---
+++ checksha.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+while true
+do
+        result="$(sha1sum /etc/nginx/landings/conf.d/*.conf | sha1sum | awk '{print $1}')"
+        # Check NGINX Configuration Test
+        # Only Reload NGINX If NGINX Configuration Test Pass
+         if [ ! -f /tmp/sha1.txt ]; then
+            #result=$(</tmp/sha1.txt)
+            echo "$result" > /tmp/sha1.txt >/dev/stdout 2>&1
+         else
+           oldvalue=$(</tmp/sha1.txt)
+         fi
+
+         sleep 5
+
+         if [ "$result" != "$oldvalue" ]; then
+           echo $result>/dev/stdout 2>&1
+           echo $oldvalue>/dev/stdout 2>&1
+
+           nginx -t
+           if [ $? -eq 0 ];then
+             echo Configuration"Configuration Changing, Reloading Nginx...">/dev/stdout 2>&1
+
+             service nginx reload
+           else
+             echo "Test Nginx Configuration failed...">/dev/stdout 2>&1
+           fi
+           echo $result > /tmp/sha1.txt
+         fi
+done
